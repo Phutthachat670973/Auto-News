a9b3d48fc2b1b15dcbb70db407798341c1ff443c
phutthachat1001 <phutthachat1001@gmail.com>
2025-06-27 14:59:04 +0700

    Update news.py

--- news.py
+++ news.py
@@ -11,7 +11,7 @@ from dateutil import parser as dateutil_parser
 from pathlib import Path
 
 # ------------------- ตั้งค่าโมเดล -------------------
-summarizer = pipeline("summarization", model="google/pegasus-cnn_dailymail")
+summarizer = pipeline("summarization", model="facebook/bart-large-mnli")
 classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
 # ------------------- ตั้งค่า API -------------------
