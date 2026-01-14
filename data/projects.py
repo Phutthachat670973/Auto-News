4eddefa4dbf872ddc770d9916bb6325eadc77619
Phutthachat670973 <ch670973@pttplc.com>
2026-01-14 09:45:37 +0700

    Create projects.py

---
+++ data/projects.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Project Database
+ฐานข้อมูลโครงการพลังงานแยกตามประเทศ
+"""
+
+PROJECTS_BY_COUNTRY = {
+    "Thailand": [
+        "โครงการจี 1/61", "โครงการจี 2/61", "โครงการอาทิตย์", "Arthit",
+        "โครงการเอส 1", "S1", "โครงการสัมปทาน 4", "Contract 4",
+        "โครงการพีทีทีอีพี 1", "PTTEP 1", "โครงการบี 6/27",
+        "โครงการแอล 22/43", "โครงการอี 5", "E5",
+        "โครงการจี 4/43", "โครงการสินภูฮ่อม", "Sinphuhorm",
+        "โครงการบี 8/32", "B8/32", "9A", "9เอ",
+        "โครงการจี 4/48", "โครงการจี 12/48",
+        "โครงการจี 1/65", "โครงการจี 3/65",
+        "โครงการแอล 53/43", "โครงการแอล 54/43"
+    ],
+    "Myanmar": [
+        "โครงการซอติก้า", "Zawtika", 
+        "โครงการยาดานา", "Yadana", 
+        "โครงการเมียนมา เอ็ม 3", "Myanmar M3"
+    ],
+    "Malaysia": [
+        "Malaysia SK309", "SK309", 
+        "Malaysia SK311", "SK311", 
+        "Malaysia Block H", "Block H"
+    ],
+    "Vietnam": [
+        "โครงการเวียดนาม 16-1", "Vietnam 16-1", "16-1", 
+        "Block B", "48/95"
+    ],
+    "Indonesia": [
+        "โครงการนาทูน่า ซี เอ", "Natuna Sea A"
+    ],
+    "Kazakhstan": [
+        "โครงการดุงกา", "Dunga"
+    ],
+    "Oman": [
+        "Oman Block 61", "Block 61", 
+        "Oman Block 6", "PDO"
+    ],
+    "UAE": [
+        "Abu Dhabi Offshore 1", 
+        "Abu Dhabi Offshore 2", 
+        "Abu Dhabi Offshore 3"
+    ],
+    "International": [
+        "OPEC", "IEA", "WTI", "Brent", "Global Energy Market"
+    ]
+}
